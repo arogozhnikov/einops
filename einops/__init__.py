29de9472bde842511c5b4c507ab98a38a463347a
Alex Rogozhnikov <iamfullofspam@gmail.com>
2018-11-14 10:52:48 -0800

    add __all__ to minimize star-imports

--- einops/__init__.py
+++ einops/__init__.py
@@ -1,4 +1,5 @@
 __author__ = 'Alex Rogozhnikov'
 __version__ = '0.1'
+__all__ = ['rearrange', 'reduce', 'parse_shape', 'asnumpy', 'EinopsError']
 
 from .einops import rearrange, reduce, parse_shape, asnumpy, EinopsError
