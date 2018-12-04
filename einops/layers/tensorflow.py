68f7df85b89ce3693ed9b6f154562f41a2f6bc95
Adam Kowalski <adam.kowalski.work@gmail.com>
2018-12-04 13:17:47 -0800

    tensorflow layers

---
+++ einops/layers/tensorflow.py
@@ -0,0 +1,35 @@
+from tensorflow.keras.layers import Layer
+
+from .._backends import UnknownSize
+from . import RearrangeMixin, ReduceMixin
+
+__author__ = 'Alex Rogozhnikov'
+
+
+class Rearrange(RearrangeMixin, Layer):
+    def compute_output_shape(self, input_shape):
+        input_shape = tuple(UnknownSize() if d.value is None else int(d) for d in input_shape)
+        init_shapes, reduced_axes, axes_reordering, final_shape = self.recipe().reconstruct_from_shape(input_shape)
+        final_shape = tuple(None if isinstance(d, UnknownSize) else int(d) for d in final_shape)
+        return final_shape
+
+    def call(self, inputs):
+        return self._apply_recipe(inputs)
+
+    def get_config(self):
+        return {'pattern': self.pattern, **self.axes_lengths}
+
+
+class Reduce(ReduceMixin, Layer):
+    def compute_output_shape(self, input_shape):
+        input_shape = tuple(UnknownSize() if d.value is None else int(d) for d in input_shape)
+        init_shapes, reduced_axes, axes_reordering, final_shape = self.recipe().reconstruct_from_shape(input_shape)
+        final_shape = tuple(None if isinstance(d, UnknownSize) else int(d) for d in final_shape)
+        return final_shape
+
+    def call(self, inputs):
+        return self._apply_recipe(inputs)
+
+    def get_config(self):
+        return {'pattern': self.pattern, 'reduction': self.reduction, **self.axes_lengths}
+
