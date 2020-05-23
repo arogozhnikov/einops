6b6b84bb6f3a45429299f47f05162e7019aaf6b9
Alex Rogozhnikov <iamfullofspam@gmail.com>
2020-05-22 21:46:54 -0700

    expose repeat operation in API

--- einops/__init__.py
+++ einops/__init__.py
@@ -2,4 +2,4 @@ __author__ = 'Alex Rogozhnikov'
 __version__ = '0.2.0'
 __all__ = ['rearrange', 'reduce', 'parse_shape', 'asnumpy', 'EinopsError']
 
-from .einops import rearrange, reduce, parse_shape, asnumpy, EinopsError
+from .einops import rearrange, reduce, repeat, parse_shape, asnumpy, EinopsError
