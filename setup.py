e31c97569b5a01ccc1647f0a96452bee5843970e
Alex Rogozhnikov <iamfullofspam@gmail.com>
2021-08-31 13:49:29 -0700

    bump version again

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="einops",
-    version='0.3.1',
+    version='0.3.2',
     description="A new flavour of deep learning operations",
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
