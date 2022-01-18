3bbf32a3c50a050c8fbdc4fb27516ca5b465f4e8
Alex Rogozhnikov <iamfullofspam@gmail.com>
2022-01-17 23:23:33 -0800

    update versions for release 0.4.0

--- einops/__init__.py
+++ einops/__init__.py
@@ -1,5 +1,5 @@
 __author__ = 'Alex Rogozhnikov'
-__version__ = '0.3.2'
+__version__ = '0.4.0'
 
 
 class EinopsError(RuntimeError):
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="einops",
-    version='0.3.2',
+    version='0.4.0',
     description="A new flavour of deep learning operations",
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
