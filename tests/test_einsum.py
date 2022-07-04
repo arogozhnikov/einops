b1676190ff1d0e17bb937b2ae38aa95ff3c0c828
MilesCranmer <miles.cranmer@gmail.com>
2022-07-03 23:59:25 -0400

    Add tests for functional einsum

--- tests/test_einsum.py
+++ tests/test_einsum.py
@@ -1,4 +1,5 @@
 from . import collect_test_backends
+from einops.einops import _compatify_pattern_for_einsum
 import numpy as np
 
 
@@ -40,6 +41,53 @@ test_layer_cases = [
 ]
 
 
+# Each of the form:
+# (Arguments, true_einsum_pattern, in_shapes, out_shape)
+test_functional_cases = [
+    (
+        # Basic:
+        Arguments("b c h w, b w -> b h"),
+        "abcd,ad->ac",
+        ((2, 3, 4, 5), (2, 5)),
+        (2, 4),
+    ),
+    (
+        # Three tensors:
+        Arguments("b c h w, b w, b c -> b h"),
+        "abcd,ad,ab->ac",
+        ((2, 3, 40, 5), (2, 5), (2, 3)),
+        (2, 40),
+    ),
+    (
+        # Ellipsis, and full names:
+        Arguments("... one two three, three four five -> ... two five"),
+        "...abc,cde->...be",
+        ((32, 5, 2, 3, 4), (4, 5, 6)),
+        (32, 5, 3, 6),
+    ),
+    (
+        # Ellipsis at the end:
+        Arguments("one two three ..., three four five -> two five ..."),
+        "abc...,cde->be...",
+        ((2, 3, 4, 32, 5), (4, 5, 6)),
+        (3, 6, 32, 5),
+    ),
+    (
+        # Ellipsis on multiple tensors:
+        Arguments("... one two three, ... three four five -> ... two five"),
+        "...abc,...cde->...be",
+        ((32, 5, 2, 3, 4), (32, 5, 4, 5, 6)),
+        (32, 5, 3, 6),
+    ),
+    (
+        # One tensor, and underscores:
+        Arguments("first_tensor second_tensor -> first_tensor"),
+        "ab->a",
+        ((5, 4),),
+        (5,),
+    )
+]
+
 
 def test_layer():
     for backend in collect_test_backends(layers=True, symbolic=False):
@@ -53,6 +101,27 @@ def test_layer():
                 output_framework = layer(input_framework)
                 output = backend.to_numpy(output_framework)
                 assert output.shape == out_shape
+
+
+def test_functional():
+    # Functional tests:
+    for backend in collect_test_backends():
+        if backend.framework_name in ['tensorflow', 'torch', 'jax', 'numpy']:
+            for args, true_pattern, in_shapes, out_shape in test_functional_cases:
+                print(f"Running '{args.args[0]}' for {backend.framework_name}")
+                predicted_pattern = args(_compatify_pattern_for_einsum)
+                assert predicted_pattern == true_pattern
+                in_arrays = [
+                    np.random.uniform(size=shape).astype('float32')
+                    for shape in in_shapes
+                ]
+                in_arrays_framework = [
+                    backend.from_numpy(array)
+                    for array in in_arrays
+                ]
+                out_array = backend.einsum(predicted_pattern, *in_arrays_framework)
+                assert out_array.shape == out_shape
+
 
 # mxnet/gluon do not support einsum without changing to numpy. which doesn't work with the rest
 # in future, after gluon migrated to a new codebase, all testing code will be moved to a new setup
