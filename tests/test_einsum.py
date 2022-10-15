5e9ed859007155cb265bd80272a3d1f04df75d87
jm12138 <2286040843@qq.com>
2022-10-16 01:46:29 +0800

    add tests

--- tests/test_einsum.py
+++ tests/test_einsum.py
@@ -166,7 +166,7 @@ test_functional_cases = [
 
 def test_layer():
     for backend in collect_test_backends(layers=True, symbolic=False):
-        if backend.framework_name in ['tensorflow', 'torch', 'chainer', 'oneflow']:
+        if backend.framework_name in ['tensorflow', 'torch', 'chainer', 'oneflow', 'paddle']:
             layer_type = backend.layers().EinMix
             for args, in_shape, out_shape in test_layer_cases:
                 layer = args(layer_type)
@@ -179,7 +179,7 @@ def test_layer():
 
 
 valid_backends_functional = ['tensorflow', 'torch', 'jax', 'numpy',
-                             'chainer', 'oneflow', 'cupy', 'tensorflow.keras']
+                             'chainer', 'oneflow', 'cupy', 'tensorflow.keras', 'paddle']
 
 def test_functional():
     # Functional tests:
