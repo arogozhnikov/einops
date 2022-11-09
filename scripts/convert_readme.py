5e0bb81d4b9d1fbd12fca1035fb28d8f82ab344e
Alex Rogozhnikov <iamfullofspam@gmail.com>
2022-11-08 23:10:08 -0500

    remove video from documentation

--- scripts/convert_readme.py
+++ scripts/convert_readme.py
@@ -9,10 +9,11 @@ original_text = Path(__file__).parent.parent.joinpath('README.md').read_text(enc
 def replace_with_video_tag(line: str):
     if line.startswith('https://') and line.endswith('.mp4') and ' ' not in line:
         # treating as link to mp4 file.
-        return f"""
-        <video width="800" controls><source src="{line}" type="video/mp4"> 
-        Your browser does not support the video </video>\n\n<br />\n\n<br />
-        """.strip()
+        return ""
+        # return f"""
+        # <video width="800" controls><source src="{line}" type="video/mp4">
+        # Your browser does not support the video </video>\n\n<br />\n\n<br />
+        # """.strip()
     else:
         # other lines are not touched
         return line
