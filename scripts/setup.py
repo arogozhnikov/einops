fec0613dcd99e7d90b1fa7bfdb630fdc505258eb
Alex Rogozhnikov <arogozhnikov@users.noreply.github.com>
2022-12-04 09:41:21 -0800

    add a mock setup script

---
+++ scripts/setup.py
@@ -0,0 +1,27 @@
+"""
+This is a fake script, it is not used.
+Seems github does not count contributions unless you have a setup.py
+"""
+__author__ = 'Alex Rogozhnikov'
+
+from setuptools import setup
+
+setup(
+    name="einops",
+    version='0.6.0',
+    description="A new flavour of deep learning operations",
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/arogozhnikov/einops',
+    author='Alex Rogozhnikov',
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3 ',
+        'License :: OSI Approved :: MIT License',
+    ],
+    keywords='deep learning, neural networks, tensor manipulation, machine learning, '
+             'scientific computations, einops',
+    install_requires=[
+        # no run-time or installation-time dependencies
+    ],
+)
